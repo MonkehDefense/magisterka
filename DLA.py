aab90471eb7e3d640d7c93ea2df0444fa5d8aec8
MonkehDefense <74372783+MonkehDefense@users.noreply.github.com>
2022-12-20 18:54:25 +0100

    manual 1.2

--- DLA.py
+++ DLA.py
@@ -383,46 +383,98 @@ class DLA_manual(nn.Module):
 			block(chan[3],chan[3]),
 			block(chan[3],chan[3])])
 
+		self.blocks_3 = nn.ModuleList([
+			block(chan[3],chan[4],2),
+			block(chan[4],chan[4]),
+			block(chan[4],chan[4]),
+			block(chan[4],chan[4]),
+			block(chan[4],chan[4]),
+			block(chan[4],chan[4]),
+			block(chan[4],chan[4]),
+			block(chan[4],chan[4])])
 
-		self.aggr_1 = Aggr_block(2*chan[2],chan[2], kernel_size=1, residual=False)# sekcja/drzewo 1
+		self.blocks_4 = nn.ModuleList([
+			block(chan[4],chan[5],2),
+			block(chan[5],chan[5])])
+
+		self.aggr_1 = Aggr_block(2*chan[2],chan[2], kernel_size=1, residual=False)
 
 		self.aggr_2 = nn.ModuleList([
-			Aggr_block(2*chan[3],chan[3], kernel_size=1, residual=False),# sekcja/drzewo 2
+			Aggr_block(2*chan[3],chan[3], kernel_size=1, residual=False),
 			Aggr_block(chan[2]+3*chan[3],chan[3], kernel_size=1, residual=False)])
+		
+		self.aggr_3 = nn.ModuleList([
+			Aggr_block(2*chan[4],chan[4], kernel_size=1, residual=False),
+			Aggr_block(3*chan[4],chan[4], kernel_size=1, residual=False),
+			Aggr_block(2*chan[4],chan[4], kernel_size=1, residual=False),
+			Aggr_block(4*chan[4]+chan[3],chan[4], kernel_size=1, residual=False)])
 
+		self.aggr_4 = Aggr_block(2*chan[5] + chan[4],chan[2], kernel_size=1, residual=False)
+  
 		self.downsample = nn.MaxPool1d(1,2)
-		self.projection1 = Conv(8,16,1)
-		self.projection2 = Conv(16,32,1)
+		self.projection1 = Conv(chan[1],chan[2],1)
+		self.projection2 = Conv(chan[2],chan[3],1)
+		self.projection3 = Conv(chan[3],chan[4],1)
+		self.projection4 = Conv(chan[4],chan[5],1)
 
 
 	def forward(self, x):
 		x = self.base_layer(x)
 
+		# sekcja 1
 		res = self.projection1(self.downsample(x))
 		b1 = self.blocks_1[0](x, res)
 		b2 = self.blocks_1[1](b1)
 		a1 = self.aggr_1(b1,b2)
-		
+
+
+		# sekcja 2
 		# print('projekcja jest ok')
 		down = self.downsample(a1)
 		# print(res.shape)
-		res = self.projection2(down)
-		
+		res = self.projection2(down)		
 
-		print(a1.shape)
+		# print(a1.shape)
 		# print(res.shape)
 		b3 = self.blocks_2[0](a1, res)
 		b4 = self.blocks_2[1](b3)
 		a2 = self.aggr_2[0](b3,b4)
 		b5 = self.blocks_2[2](a2)
 		b6 = self.blocks_2[3](b5)
-
 		# print(b6.shape)
 		a3 = self.aggr_2[1](down,a2,b5,b6)
 
 
 
-		out = a3
+		# sekcja 3
+		down = self.downsample(a3)
+		# print(res.shape)
+		res = self.projection3(down)
+
+		b7 = self.blocks_3[0](a3,res)
+		b8 = self.blocks_3[1](b7)
+		a4 = self.aggr_3[0](b7,b8)
+
+		b9 = self.blocks_3[2](a4)
+		b10 = self.blocks_3[3](b9)
+		a5 = self.aggr_3[1](a4, b9, b10)
+
+		b11 = self.blocks_3[4](a5)
+		b12 = self.blocks_3[5](b11)
+		a6 = self.aggr_3[2](b11,b12)
+
+		b13 = self.blocks_3[6](a6)
+		b14 = self.blocks_3[7](b13)
+		a7 = self.aggr_3[3](down, a5, a6, b13, b14)
+  
+		# sekcja 4
+		down = self.downsample(a7)
+		res = self.projection4(down)
+
+		b15 = self.blocks_4[0](a7,res)
+		b16 = self.blocks_4[1](b15)
+		out = self.aggr_4(down, b15, b16)
+
 		return out
 
 
